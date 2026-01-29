fe979cc07f78acc7236506fa57e927d724eb16ea
eiweiw321 <eiweiw321@gmail.com>
2026-01-29 23:24:41 +0700

    add file

---
+++ BST_min_max.py
@@ -0,0 +1,106 @@
+class BSTNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, data):
+        if self.root is None:
+            self.root = BSTNode(data)
+            return
+
+        now = self.root
+        while True:
+            if data < now.data:
+                if now.left is None:
+                    now.left = BSTNode(data)
+                    return
+                now = now.left
+            else:
+                if now.right is None:
+                    now.right = BSTNode(data)
+                    return
+                now = now.right
+
+    def preorder(self):
+        def _pre(node):
+            if node is None:
+                return
+            print(" -> " + str(node.data), end="")
+            _pre(node.left)
+            _pre(node.right)
+
+        _pre(self.root)
+        print(" ")
+    
+    def is_empty(self):
+        return self.root is None
+    
+    def inorder(self):
+        def _in(node):
+            if node is None:
+                return
+            _in(node.left)
+            print(" -> " + str(node.data) ,end="")
+            _in(node.right)
+        _in(self.root)
+        print(" ")
+
+    def postorder(self):
+        def _post(node):
+            if node is None:
+                return
+            
+            _post(node.left)
+            _post(node.right)
+            print(" -> " + str(node.data), end="")
+        _post(self.root)
+        print(" ")
+    def traverse(self):
+        if self.is_empty():
+            print("This is an empty binary search tree.")
+            return
+        
+        print("Preorder:",end="")
+        self.preorder()
+
+        print("Inorder:" , end="")
+        self.inorder()
+
+        print("Postorder:" , end="")
+        self.postorder()
+
+    def find_min(self):
+        if self.is_empty():
+            return None
+        
+        now = self.root
+
+        while now.left is not None:
+            now = now.left
+        return now.data
+    
+    def find_max(self):
+        if self.is_empty():
+            return None
+        
+        now = self.root
+
+        while now.right is not None:
+            now = now.right
+        return now.data
+    
+def main():
+    my_bst = BST()
+    for i in range(int(input())):
+        my_bst.insert(int(input()))
+    my_bst.traverse()
+    print("Max:", my_bst.find_max())
+    print("Min:", my_bst.find_min())
+
+main()
